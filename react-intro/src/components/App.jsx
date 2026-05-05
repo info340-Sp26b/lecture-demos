ec5688fb3bebf1382985b55216b52c79c1e0c561
Tim Carlson <timca@arenasports.net>
2026-05-05 11:37:03 -0700

    post lecture intro-react push

---
+++ react-intro/src/components/App.jsx
@@ -0,0 +1,11 @@
+import {HeaderComponent} from './HeaderComponent.jsx'
+import {PetList} from './PetList.jsx'
+
+export function App(props) {
+    return (
+        <>
+            <HeaderComponent />
+            <PetList />
+        </>
+    )
+}
---
+++ react-intro/src/components/HeaderComponent.jsx
@@ -0,0 +1,8 @@
+export function HeaderComponent(props) {
+    return (
+        <div>
+            <header>
+                <h1>React Pet Header</h1>
+            </header>
+        </div>);
+}
